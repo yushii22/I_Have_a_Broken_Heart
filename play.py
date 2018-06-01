99750992ea74ffa5ee56a36d08c9cc7e1fdb099b
JJC <jjc@nlplab.cc>
2018-06-01 16:31:38 +0800

    add play.py

---
+++ play.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from game import Game
+from agent import AgentRandom
+
+
+if __name__ == '__main__':
+    game = Game([AgentRandom('Iron Man'), AgentRandom('Superman'), AgentRandom('Batman'), AgentRandom('Spiderman')])
+
+    game.set_game()
+    scores = game.play()
