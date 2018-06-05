1322f48dd00bb652d1c9fdb5daee82e4e56fd3fc
Ｍing Chiao <joe@MacBook-Pro-3.local>
2018-06-05 14:54:18 +0800

    add exception

---
+++ exception.py
@@ -0,0 +1,13 @@
+
+class PassCardIllegalException(Exception):
+    def __init__(self):
+        self.message = "Passing Cards not in your hand!"
+    def __str__(self):
+        return self.message
+
+
+class IllegalMoveExceptioin(Exception):
+    def __init__(self):
+        self.message = "Playing Card not in your hand!"
+    def __str__(self):
+        return self.message
