6091ce1c4e95abbe0f43f44ec862bbf9545e9c15
106071015 <37043933+yushii22@users.noreply.github.com>
2018-06-09 23:47:22 +0800

    Update my_agent.py

--- agent/my_agent.py
+++ agent/my_agent.py
@@ -1,24 +1,113 @@
 # -*- coding: utf-8 -*-
 from agent import Agent
-from card import ALL_CARDS
+import random
+from game import Game
+
 
 
 class MyAgent(Agent):
-    '''
-    You have to decide which card to play in this round.
-    cards_you_have: list of cards in your hand
-    cards_played: cards that has been played this round
-    heart_broken: heart is broken or not
-    info: score information and cards played in previous rounds
-    '''
     def play(self, cards_you_have, cards_played, heart_broken, info):
-        return ...
+        legal_moves = Game.get_legal_moves(cards_you_have, cards_played, heart_broken)
+        return self.get_good_moves(cards_you_have, cards_played, heart_broken, legal_moves)
 
-    '''
-    decide cards you want to pass to the player next to you
-    0->1, 1->2, 2->3, 3->0
-
-    cards: list of cards in your hand
-    '''
     def pass_cards(self, cards):
-        return ...
+        spade = 0
+        diamond = 0
+        club = 0
+        spade_list = []
+        diamond_list = []
+        club_list = []
+        cards_list = []
+        for card in cards:
+            cards_list.append(card)
+        for card in cards:
+            if card.suit == '♠':
+                spade += 1
+                spade_list.append(card)
+            elif card.suit == '♦':
+                diamond += 1
+                diamond_list.append(card)
+            elif card.suit == '♣':
+                club += 1
+                club_list.append(card)
+            else:
+                heart = 0
+        cards_should_pass = []
+        if len(spade_list) <= 2:
+            cards_should_pass = cards_should_pass + spade_list
+            
+        if len(diamond_list) <= 2:
+            cards_should_pass = cards_should_pass + diamond_list
+            
+        if len(club_list) <= 2:
+            cards_should_pass = cards_should_pass + club_list
+            
+        if len(cards_should_pass) >= 3:
+            return random.sample(cards_should_pass, 3)
+        elif int(len(cards_should_pass)) == 2:
+            a = cards_should_pass[0]
+            cards_list.remove(a)
+            b = cards_should_pass[1]
+            cards_list.remove(b)
+            c = random.choice(cards_list)
+            cards_should_pass.append(c)
+            return cards_should_pass
+        elif int(len(cards_should_pass)) == 1:
+            a = cards_should_pass[0] 
+            cards_list.remove(a)
+            b = random.choice(cards_list)
+            cards_list.remove(b)
+            cards_should_pass.append(b)
+            c = random.choice(cards_list)
+            cards_should_pass.append(c)
+            return cards_should_pass
+        else:
+            return random.sample(cards, 3)
+
+    def score(self, cards):
+        total_score = 0
+        for card in cards:
+            if card.suit == '♥':
+                total_score += 1
+            elif card == '♠12':
+                total_score += 13
+        return total_score
+
+    def get_good_moves(self, cards_you_have, cards_played, heart_broken, legal_moves):
+        if not cards_played:
+            all_numbers = [int(card.number) for card in legal_moves]
+            min_number = min(all_numbers)
+            result = []
+            for card in legal_moves:
+                if card.number == min_number:
+                    result.append(card)
+            return random.choice(result)
+        else:
+            suit = cards_played[0].suit
+            all_numbers = [int(card.number) for card in cards_played if card.suit == suit]
+            max_number = 1 if 1 in all_numbers else max(all_numbers)
+            card_score = {}
+            for card in legal_moves:
+                card_num = int(card.number)
+                card_suit = card.suit
+                total_score = self.score(cards_played + [card])
+                if ((card_suit == suit and card_num == 1) 
+                    or (card_suit == suit and card_num > max_number and max_number != 1)
+                   ):
+                    card_score[card] = total_score
+                else:
+                    card_score[card] = 0
+            good_moves = []
+            good_moves = [card for card in legal_moves if card_score[card] == min(card_score.values())]
+            all__numbers = [int(card.number) for card in good_moves]
+            max__number = 1 if 1 in all__numbers else max(all__numbers)
+            result = []
+            for card in good_moves:
+                card_num = int(card.number)
+                if card_num == max__number:
+                    result.append(card)
+            return random.choice(result)
+
+
+        
+        
